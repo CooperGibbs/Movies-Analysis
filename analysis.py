218d2a1dbe102870b063cfd35ee99dfd3c3ee743
CooperGibbs <coopergibbs@Coopers-MacBook-Pro.local>
2023-04-13 17:49:35 -0400

    Completed steps
    Added code to find top N most rated movies; intermediate step was to join movies table with ratings data via movieID

--- analysis.py
+++ analysis.py
@@ -1,9 +1,24 @@
 import pandas as pd
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as FUN
+#PYSPARK_DRIVER_PYTHON = 3.85
+#PYSPARK_PYTHON = 3.85
+import os
+import sys
+
+os.environ['PYSPARK_PYTHON'] = sys.executable
+os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
+
+# Same code as shown in SimpleApp.py
+
+
 class MovieAnalyzer:
     
     def __init__(self):
-        self.movie = None
+        pass
 
-    def open_movie_data(self):
-        self.movie = pd.read_csv("data/movies.csv")
-        return self.movie
+    def open_movie_data(self, movie_data):
+        #self.movie = pd.read_csv("data/movies.csv")
+        #return self.movie
+        print(movie_data.head(5))
+        return self
